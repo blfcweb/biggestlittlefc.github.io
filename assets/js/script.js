e9d420e33f6c34d910bb6f5b352ac92d3cfc0f1a
Unknown <website@goblfc.org>
2017-11-09 08:00:16 -0800

    und stab 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,16 @@ jQuery(document).ready(function($) {
 
 	$('#menu-main .menu-group ul').slideUp(0);
 	$('#menu-main .menu-group h3').click(function() {
-		// if this sibling has class  '.menu-open', slide up, remove class
+
+		var $self = $(this).siblings('ul');
+		// if this sibling hasclass, slide up, remove class
 		// else sliderup .menu-opens, remove class. Then slidedown current, add class.
-		$('.menu-open').not( this + ' .menu-open').slideUp(400).removeClass('menu-open');
-		$(this).siblings('ul').slideDown(400).addClass('menu-open');
+		if ( $self.hasClass('menu-open') ) {
+			$self.slideUp(400).removeClass('menu-open');
+		} else {
+			$('.menu-open').slideUp(400).removeClass('menu-open');
+			$self.slideDown(400).addClass('menu-open');
+		}
 	});
 
 });
