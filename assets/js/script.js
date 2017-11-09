ccd8d77e6981e3c6661af9fa01cf34f57a18ee10
Unknown <website@goblfc.org>
2017-11-08 22:25:16 -0800

    toggle other open/close menus

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function($) {
 
 	$('#menu-main .menu-group ul').slideUp(0);
 	$('#menu-main .menu-group h3').click(function() {
-		$(this).siblings('ul').slideToggle(400);
+		$('.menu-open').slideUp(400).removeClass('menu-open');
+		$(this).siblings('ul').slideToggle(400).addClass('menu-open');
 		console.log("Menu Toggled");
 	});
 
