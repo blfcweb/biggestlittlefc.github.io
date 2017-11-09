d2c58ee95588769ba160c101af238fa594f9775a
Unknown <website@goblfc.org>
2017-11-08 22:42:49 -0800

    fix infinite accordion menu stab 1..

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,7 @@ jQuery(document).ready(function($) {
 	$('#menu-main .menu-group ul').slideUp(0);
 	$('#menu-main .menu-group h3').click(function() {
 		$('.menu-open').slideUp(400).removeClass('menu-open');
-		$(this).siblings('ul').slideToggle(400).addClass('menu-open');
-		console.log("Menu Toggled");
+		$(this).siblings('ul').not('.menu-open').slideDown(400).addClass('menu-open');
 	});
 
 });
