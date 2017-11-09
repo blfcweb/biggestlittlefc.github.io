2ae252b39ee82dab76d87b0b86959760f34fc0f3
Unknown <website@goblfc.org>
2017-11-08 22:11:07 -0800

    Add Submenu accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function($) {
 
+	$('#menu-main .menu-group ul').slideUp(0);
+	$('#menu-main .menu-group h3').click(function() {
+		$(this).siblings('ul').slideToggle(400);
+		console.log("Menu Toggled");
+	});
 
 });
