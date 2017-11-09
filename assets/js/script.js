1e4e39d9c93db669f31bce7e4f1847cf3f02d637
Unknown <website@goblfc.org>
2017-11-08 23:07:11 -0800

    stab 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@ jQuery(document).ready(function($) {
 
 	$('#menu-main .menu-group ul').slideUp(0);
 	$('#menu-main .menu-group h3').click(function() {
-		$('.menu-open').slideUp(400).removeClass('menu-open');
-		$(this).siblings('ul').not('.menu-open').slideDown(400).addClass('menu-open');
+		// if this sibling has class  '.menu-open', slide up, remove class
+		// else sliderup .menu-opens, remove class. Then slidedown current, add class.
+		$('.menu-open').not( this + ' .menu-open').slideUp(400).removeClass('menu-open');
+		$(this).siblings('ul').slideDown(400).addClass('menu-open');
 	});
 
 });
