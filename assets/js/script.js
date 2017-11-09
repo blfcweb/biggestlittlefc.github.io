850c3b81df0081f44bf6e66b937d7b35c21240ed
Unknown <website@goblfc.org>
2017-11-09 08:09:22 -0800

    remove logic notes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,7 @@ jQuery(document).ready(function($) {
 
 	$('#menu-main .menu-group ul').slideUp(0);
 	$('#menu-main .menu-group h3').click(function() {
-
 		var $self = $(this).siblings('ul');
-		// if this sibling hasclass, slide up, remove class
-		// else sliderup .menu-opens, remove class. Then slidedown current, add class.
 		if ( $self.hasClass('menu-open') ) {
 			$self.slideUp(400).removeClass('menu-open');
 		} else {
